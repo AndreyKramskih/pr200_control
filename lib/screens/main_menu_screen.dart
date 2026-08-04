ec00c5a817c9dd470b05d766b7a707b3d687e763
Andrey Kramskih <andreykramskih@gmail.com>
2026-08-04 11:33:51 +0300

    Changed number of verdion to 1.0.3

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -545,7 +545,7 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                           );
                         },
                         child: Text(
-                          'PR200 v1.0.2',
+                          'PR200 v1.0.3',
                           style: TextStyle(
                             fontSize: 12,
                             color: themeProvider.isDarkMode
